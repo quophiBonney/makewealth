8dfdfd1bbcd0437eab25edfb3069b6ba6f0f0264
bonney <quophiclef@gmail.com>
2023-04-15 23:16:23 -0700

    okay

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import {Container, Row, Col, Button} from "react-bootstrap";
 import cheap from "../images/cheap.png";
-import {BsFillPeopleFill} from "react-icons/bs";
+import {BsCashCoin, BsFillPeopleFill} from "react-icons/bs";
 import Carousel from "react-bootstrap/Carousel";
 const Main = () => {
   return (
@@ -22,15 +22,15 @@ const Main = () => {
               Investing with Our Bank.
             </h1>
             <p className="subText">
-              We are the multinational banking firm that gives you more than{" "}
+              We are the multinational banking firm that gives you more than
               <strong>10%</strong> on your investment without any risk.
             </p>
             <a
               href="https://wa.me/+233245153283"
               className="btn btn-enquiries mt-2"
             >
-              Start Your Investment
-              <BsFillPeopleFill className="m-2" />
+              Start Investing
+              <BsCashCoin className="m-2" />
             </a>
           </Col>
           <Col
@@ -44,7 +44,7 @@ const Main = () => {
           >
             <img
               src={cheap}
-              className="img-fluid img-responsiven carousel-img"
+              className="img-fluid img-responsive carousel-img"
               alt=""
             />
           </Col>
